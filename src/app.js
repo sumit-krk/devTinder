a57b88a07eeb6ec59c683a7245febc4418e777ab
sumit-krk <kushakk007@gmail.com>
2024-09-18 22:40:14 +0530

    explore get, post, put and delete

--- src/app.js
+++ src/app.js
@@ -2,8 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res)=>{
-    res.send("Hello from node js")
+app.get("/user", (req,res)=>{
+    res.send({firstName: "sumit", lastName:"Kumar"})
+})
+
+app.post("/user", (req,res)=>{
+    res.send("Data Send SuccesFully")
+})
+
+app.delete("/user", (req,res)=>{
+    res.send("user Deleted Successfully")
+})
+
+app.put("/user", (req,res)=>{
+    res.send("user Data Updated Successfully")
 })
 
 app.listen(3000, ()=>{
