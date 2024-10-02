7ef02ecc377f910cfb47ea569a4d3878bd9ea0ad
sumit kumar <sumit.kumar@marktine.com>
2024-10-03 00:38:16 +0530

    playing with the routes.

--- src/app.js
+++ src/app.js
@@ -2,8 +2,16 @@ const express = require("express");
 
 const app = express();
 
+//route order important in node.js and also exact match of route
+
 app.get("/user", (req,res)=>{
+    console.log(req.query)
     res.send({firstName: "sumit", lastName:"Kumar"})
+})
+
+app.get("/subject/:name/:teacher",(req,res)=>{
+    console.log(req.params)
+    res.send("get Teacher Details")
 })
 
 app.post("/user", (req,res)=>{
@@ -18,7 +26,7 @@ app.put("/user", (req,res)=>{
     res.send("user Data Updated Successfully")
 })
 
-app.listen(3000, ()=>{
+app.listen(7000, ()=>{
     console.log("server listning on port 3000")
 })
 
